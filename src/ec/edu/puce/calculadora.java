0a3e6990128e5a04ceda8df7492deb4b8577ac34
Jimy <maicolluje45@gmail.com>
2026-07-03 21:46:51 -0500

    integracion del menu

--- src/ec/edu/puce/calculadora.java
+++ src/ec/edu/puce/calculadora.java
@@ -1,5 +1,22 @@
 package ec.edu.puce;
 
+import java.util.Scanner;
+
 public class calculadora {
 
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+        int opcion = 0;
+
+        do {
+            System.out.println("--- CALCULADORA ---");
+            System.out.println("1. Suma");
+            System.out.println("2. Resta");
+            System.out.println("3. Multiplicacion");
+            System.out.println("4. Division");
+            System.out.println("5. Salir");
+            System.out.print("Elija una opcion: ");
+        }
+    }
 }
